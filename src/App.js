7fa61f9553053a3383fb56cabccddff9a63e2d39
Ahmed Haroon <ahmedelharoon86@gmail.com>
2024-02-24 21:20:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import Layout from "./Components/Layout";
+import Home from "./Components/Home";
+import Brands from "./Components/Brands";
+import Cart from "./Components/Cart";
+import Products from "./Components/Products";
+import PageNotFound from "./Components/PageNotFound";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import AuthContextProvider from "./Contexts/AuthContext";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import ProductDetails from "./Components/ProductDetails";
+import { ToastContainer} from 'react-toastify';
+import Address from "./Components/Address";
+import Orders from "./Components/Orders";
+import AllCategories from "./Components/AllCategories";
+import ForgotPassword from "./Components/ForgotPassword";
+import VerifyCode from "./Components/VerifyCode";
+import ResetPassword from "./Components/ResetPassword";
+import AllProducts from "./Components/AllProducts";
+// import SpecificCategory from "./Components/SpecificCategory";
+
 
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout />,
+      children: [
+        { path: "", element: <Navigate to={"home"} /> },
+
+        {
+          path: "home",
+          element: (
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "brands",
+          element: (
+            <ProtectedRoute>
+              <Brands />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: "cart",
+          element: (
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          ),
+        },
+        {path: "allProducts",element: (<ProtectedRoute><AllProducts /></ProtectedRoute>),},
+        {path: "products",element: (<ProtectedRoute><Products /></ProtectedRoute>),},
+        {path: "address/:cartId",element: (<ProtectedRoute> <Address /> </ProtectedRoute>),},
+        {path: "categories",element: ( <ProtectedRoute><AllCategories /></ProtectedRoute>)},
+        {path: "allOrders",element: ( <ProtectedRoute> <Orders /> </ProtectedRoute>)},  
+        {path: "productDetails/:id",element: ( <ProtectedRoute> <ProductDetails /> </ProtectedRoute>)},
+        { path: "login", element: <Login /> },
+        { path: "passwordReset", element: <ForgotPassword /> },
+        
+        {path: "verifyCode",element: <VerifyCode />}, 
+        {path: "resetPassword",element: <ResetPassword />}, 
+        { path: "register", element: <Register /> },
+        { path: "*", element: <PageNotFound /> },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AuthContextProvider>
+        <RouterProvider router={routes}></RouterProvider>;
+      </AuthContextProvider>
+      <ToastContainer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import 'react-toastify/dist/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
